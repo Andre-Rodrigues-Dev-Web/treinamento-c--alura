5baf18217004856250a921f298dbbb0ee7874e05
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2022-06-23 17:52:54 -0300

    add files Hello World
    - arquivos hello world adicionados

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Banco ALR");
+
+// Valores e tipos de variáveis
+string titular = "André Laurentino";
+string conta = "10123-x";
+int numero_agencia = 20;
+string nome_agencia = "Agencia Bambuí";
+double saldo = 120.50;
+
+// Exibição de informação
+Console.WriteLine("Titular:" + titular);
+Console.WriteLine("Conta:" + conta);
+Console.WriteLine("Número agência:" + numero_agencia);
+Console.WriteLine("Nome agência:" + nome_agencia);
+Console.WriteLine("Saldo:" + saldo);
+
+// evento que fecha as informações
+Console.ReadKey();
